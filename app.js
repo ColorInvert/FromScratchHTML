e013402719b7267cd9e2434fc969bd7dcde14134
Casey Glidewell <caseyglidewell@gmail.com>
2022-06-25 20:01:07 -0700

    Added user verification and a basic if else, to meet the requirements of lab 6.

---
+++ app.js
@@ -0,0 +1,3 @@
+let usrname = prompt("Live and breathe, Scrapworker. Please input your name now.");
+document.write("User: " + usrname + " validated. You are now logged in, and your account and scrip values have been linked.");
+
