88ddd9b769ec09d314864e71c4eafc1aeefa4ccc
Casey Glidewell <caseyglidewell@gmail.com>
2022-06-29 16:46:58 -0700

    Added looping prompt, image addition based on user prompt, and a few other elements to hopefully meet all requirements of lab 8.

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 let usrname = "nothing";
+let rating = "NOT YET DEFINED";
+
+
+var myImage = new Image(100, 200);
+myImage.src = 'Star.png';
+
+
 function getusr(){
     let usrnamelocal = prompt("Live and breathe, Scrapworker. Please input your name now.");
     document.write("User " + usrnamelocal + " validated. You are now logged in, and your account and scrip values have been linked.");
@@ -31,7 +38,7 @@ function getcreds(){
             } 
         }
         else {
-            document.write("Demerit added to " + usrname +"'s account for willful wasting of company resources.");
+            document.write("A demerit has been added to " + usrname + "'s account for willful wasting of company resources through repeated refusal to follow instruction.");
         }
     }
 }
@@ -40,3 +47,21 @@ function getreason(){
     let reason = prompt("Please state your reason for arriving at this page.");
     document.write("User " + usrname + " has stated that \"" + reason + "\" was the reason for arrival to this page, and response has been recorded for satisfaction survey purposes.");
 }
+
+function getrating(){
+    
+    let rating = prompt("What star rating, would you give our company, RE:Fresh.?");
+        
+    while (isNaN(rating) === true) {
+        rating = prompt("Enter a number, please.");
+    }
+
+    for (let i = rating; i >= 1; i--) {
+       document.write('<img src="Star.png" width="30" height="30"</img>');
+    }
+        
+    document.write("\nRE:Fresh. A customer focused company with a " + rating + " star rating.");
+          
+}
+
+
