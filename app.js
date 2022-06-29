118a1ed7f9f9bcf9614081b3a5c67aeae8b4a728
Casey Glidewell <caseyglidewell@gmail.com>
2022-06-29 14:37:47 -0700

    Added proper validation of the second prompt receiving a numeric value, to fit last requirement for the lab.

--- app.js
+++ app.js
@@ -7,11 +7,33 @@ function getusr(){
 
 function getcreds(){
     let creds = prompt("Please enter your current funds in raw number form only.");
-    if (creds < 58) {
-        document.write("Insufficient funds for any product on offer. Scrapworker " + usrname + "'s wasting of RE:Fresh. bandwith has been noted.");
-    } else {
-        document.write("Acceptable value of scrip (¤ " + creds + ") verified. Enjoy your shopping, " + usrname + ".");
-    } 
+    if (isNaN(creds) === false) {
+        
+        if (creds < 58) {
+            document.write("Insufficient funds for any product on offer. Scrapworker " + usrname + "'s wasting of RE:Fresh. bandwith has been noted.");
+            return;
+        } else {
+            document.write("Acceptable value of scrip (¤ " + creds + ") verified. Enjoy your shopping, " + usrname + ".");
+            return;
+        } 
+    }
+    else {
+
+        creds = prompt("Invalid response. Please enter your current funds in RAW NUMBER FORM, ONLY.");
+        if (isNaN(creds) === false){
+
+            if (creds < 58) {
+                document.write("Insufficient funds for any product on offer. Scrapworker " + usrname + "'s wasting of RE:Fresh. bandwith has been noted.");
+                return;
+            } else {
+                document.write("Acceptable value of scrip (¤ " + creds + ") verified. Enjoy your shopping, " + usrname + ".");
+                return;
+            } 
+        }
+        else {
+            document.write("Demerit added to " + usrname +"'s account for willful wasting of company resources.");
+        }
+    }
 }
 
 function getreason(){
