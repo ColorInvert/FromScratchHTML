5a6ffbe334e3ca40c794adac74f42264b1bdc276
Casey Glidewell <caseyglidewell@gmail.com>
2022-06-29 14:08:20 -0700

    Consolidated script tags into an appjs section, and implemented functions.

--- app.js
+++ app.js
@@ -1,3 +1,20 @@
-let usrname = prompt("Live and breathe, Scrapworker. Please input your name now.");
-document.write("User: " + usrname + " validated. You are now logged in, and your account and scrip values have been linked.");
+let usrname = "nothing";
+function getusr(){
+    let usrnamelocal = prompt("Live and breathe, Scrapworker. Please input your name now.");
+    document.write("User " + usrnamelocal + " validated. You are now logged in, and your account and scrip values have been linked.");
+     usrname = usrnamelocal;
+}
 
+function getcreds(){
+    let creds = prompt("Please enter your current funds in raw number form only.");
+    if (creds < 58) {
+        document.write("Insufficient funds for any product on offer. Scrapworker " + usrname + "'s wasting of RE:Fresh. bandwith has been noted.");
+    } else {
+        document.write("Acceptable value of scrip (¤ " + creds + ") verified. Enjoy your shopping, " + usrname + ".");
+    } 
+}
+
+function getreason(){
+    let reason = prompt("Please state your reason for arriving at this page.");
+    document.write("User " + usrname + " has stated that \"" + reason + "\" was the reason for arrival to this page, and response has been recorded for satisfaction survey purposes.");
+}
